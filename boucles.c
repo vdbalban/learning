3789924ac4ad652284546764408e4b11aaff2e23
vdbalban <vdb.alban@gmail.com>
2026-07-15 18:31:31 +0200

    Premier push learning

--- boucles.c
+++ boucles.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-    int a = 0;
+    int a =0;
     long b =0;
     printf("\nBonjour,Entre un chiffre pour changer le monde\n");
-    scanf("%ld", &b);
+    scanf("%ld", &b); //%ld tag d'un long
     while (a < b)
         {
         a++;
-        printf("Bravo, Jeffrey Epstein vient de capturer son %deme enfants !\n", a);
+        printf("Bravo, Jeffrey Epstein vient de capturer son %ldeme enfants !\n", a);
         }
-    printf("\nSois fier de toi %d enfant sont sur l'ile !", a);
+    printf("\nSois fier de toi %ld enfant sont sur l'ile !", a);
     printf("\nEt tout ça, grace a ton choix !\n\n");
 
     return 0;
