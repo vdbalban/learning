bc88b401cce9f40e809e873d49d38059aaa8883d
vdbalban <vdb.alban@gmail.com>
2026-07-15 22:26:46 +0200

    ajout While

--- exojusteprix.c
+++ exojusteprix.c
@@ -2,9 +2,25 @@
 
 int main(void)
 {
-    int prix;
+    int prixReel = 42;
     int estimation;
     int nombreEssaie;
     
+    printf("\nBonjour dans ce jeu vous allez devoir deviner le chiffre mystere\n");
+    printf("il se situe entre 1 et 100\n");
+    printf("C'est a vous :\n\n");
+    scanf("%d", &estimation);
+    while(estimation > prixReel)
+    {
+        printf("Reesaie, c'est moins\n");
+        scanf("%d", &estimation);
+    }
+    while(estimation < prixReel)
+    {
+        printf("Reesaie, c'est plus\n");
+        scanf("%d", &estimation);
+        break;
+    }
+
     return 0;
 }
