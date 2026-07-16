bbac2e30f9f32ddd100d46ea9a4bce931bd428b5
vdbalban <vdb.alban@gmail.com>
2026-07-16 18:24:13 +0200

    Fin de projet

--- exojusteprix.c
+++ exojusteprix.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
     int prixReel = 42;
-    int estimation;
+    int estimation = 0;
     int nombreEssaie = 1;
     
-    printf("\nBonjour dans ce jeu vous allez devoir deviner le nombre mystere\n");
-    printf("il se situe entre 1 et 100\n");
-    printf("C'est a vous :\n\n");
-    scanf("%d", &estimation);
-
-    while(estimation != prixReel)
-    {       
-        while(estimation > prixReel)
-        {
-            printf("Reesaie, c'est moins !\n");
-            nombreEssaie++;
-            scanf("%d", &estimation);
-        }     
-        while(estimation < prixReel)
-        {
-            printf("Reesaie, c'est plus !\n");
-            nombreEssaie++;
-            scanf("%d", &estimation);
-            break;
-        }       
-    }
+    write(1,"\n\nBonjour dans ce jeu vous allez devoir deviner le nombre mystere\n", 67);
+    write(1, "il se situe entre 1 et 100\n", 28);
+    write(1,"Vous avez 10 tentatives !\n", 27);
+    write(1, "\nC'est a vous :\n", 17);
     
-    printf("\nFélicitation, Vous avez entrer le nombre mystere 42 !!\n");
-    printf("\nil vous aura fallu %d tentatives\n\n", nombreEssaie);
+
+    while(estimation != prixReel && nombreEssaie != 11)
+    {   
+        printf("C'est votre %d essai(s) : \n\n", nombreEssaie);   
+        scanf("%d", &estimation);
+        if (estimation > prixReel)
+        {
+            printf("\nReesaie c'est moins !\n");
+            nombreEssaie++;
+        }
+        else if (estimation < prixReel)
+        {
+            printf("\nReesaie, c'est plus !\n");
+            nombreEssaie++;
+        }
+    }
+        if (nombreEssaie == 11)
+        {
+            write(1, "\nVous avez épuisé vos 10 essais\n", 35);
+        }
+        else
+        {
+            write(1,"\nFélicitation, Vous avez entrer le nombre mystere 42 !!\n", 58);
+            printf("\nil vous aura fallu %d tentatives\n\n", nombreEssaie);
+        }
 
     return 0;
 }
