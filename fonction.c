2224b36cfa33043c51d891ffb30143b30ce7cc23
vdbalban <vdb.alban@gmail.com>
2026-07-17 16:15:38 +0200

    Fonction multi 3

--- fonction.c
+++ fonction.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main()
+int triple(int number)
 {
-    
+    int resultat = 0;
+
+    resultat = 3 * number; // Calcul du "number" fourni par 3
+    return resultat; // On return le resultat 
+}
+
+int main(void)
+{
     return 0;
 }
