d583dc84318c3d063d44fdce77d2b23da715ca4a
vdbalban <vdb.alban@gmail.com>
2026-07-17 16:26:13 +0200

    Fonction multi 3

--- fonction.c
+++ fonction.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int triple(int number)
+int triple(int number)  // fonction de type int
 {
     int resultat = 0;
 
     resultat = 3 * number; // Calcul du "number" fourni par 3
-    return resultat; // On return le resultat 
-}
+    return resultat; // poss ecrire "return 3 * nombre;" pour raccourcir la fontion
+}                    
 
 int main(void)
 {
