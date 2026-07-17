577b4c6a59cb4b05be466ae3b67e7ce9c50c911b
vdbalban <vdb.alban@gmail.com>
2026-07-17 15:30:38 +0200

    Boucle si mauvaise entree

--- exerciceburger.c
+++ exerciceburger.c
@@ -2,41 +2,37 @@
 
 int main(void)
 {
-    int choixmenu = 0;
+    int choixmenu = 1;
     printf("=== Bienvenue Chez Hamid Burger ! ===\n");
     printf("1. Royal Kabyle\n");
     printf("2. Mc Kefta\n");
     printf("3. Mc Tanjia\n");
     printf("4. Big Mouloud\n");
     printf("Merci de choisir votre menu ?\n");
-    scanf("%d", &choixmenu);
-    switch (choixmenu)
+    
+    while (choixmenu) // Peut etre sans opérateur (+, -, ==)
+    {  
+        scanf("%d", &choixmenu); // Prendre l'input dans la boucle
+        switch (choixmenu)
         {
         case 1:
         printf("Vous avez selectionne le menu (1) Royal Kabyle\n");
-        break;
+        return 0; // Sort de la fonction
         case 2:
         printf("Vous avez selectionne le menu (2) Mc Kefta\n");
-        break;
+        return 0;
         case 3:
         printf("Vous avez selectionne le menu (3) Mc Tanjia\n");
-        break;
+        return 0;
         case 4:
         printf("Vous avez selectionne le menu (4) Big Mouloud\n");
-        break;
+        return 0;
         default:
-        if (choixmenu < 4)
-            {
-                printf("Vous avez entrée une mauvaise selection\n");
-                printf("Merci de choisir votre menu ?\n");
-                scanf("%d", &choixmenu);
-            }
+        printf("Vous avez entrée une mauvaise selection\n");
+        printf("Merci de choisir votre menu ?\n");
+        break;
         }
+    }
         
-        // default:
-        // printf("Vous avez entrée une mauvaise selection\n");
-        // printf("Merci de choisir votre menu ?\n");
-        // scanf("%d", &choixmenu);
-        // }
     return 0;
 }
