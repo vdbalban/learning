bacfc6d13a5a2dd4caf08e95535cf81a76b0e9f4
vdbalban <vdb.alban@gmail.com>
2026-07-19 01:15:06 +0200

    ajout section depense

--- exodepense.c
+++ exodepense.c
@@ -1,34 +1,45 @@
 #include <stdio.h>
 
-int calculSalaire(int salaire)
+double calculSalaire(double salaire)
 {
-    printf("Quel est votre salaire ...");
-    scanf("%d", salaire);
+    printf("\nQuel est votre salaire");
+    printf("\nutilisez \".\" pour la virgule ... ");
+    scanf("%lf", &salaire);
     return salaire;
 }
 
 double calculDepense(double depense)
 {
-    printf("Saisie de la depense ...");
-    scanf("%lf", depense);
+    double a = 1; depense = 0;    
+    while (a != 0)
+    {
+        printf("\nVeuillez saisir la depense, 0 pour terminer");
+        printf("\nutilisez \".\" pour la virgule ... ");
+        scanf("%lf", &a);
+        depense = (a + depense);
+    }
+return depense;
 }
 
-double calculReste(double reste)
-{
-    // CALCUL
-}
+// double calculReste(double reste) //double resultatDepense,double resultatSalaire
+// {
+// return(resultatSalaire - resultatDepense);
+// }
 
-int calculPourcentage(int Pourcentage)
-{
-    \\ Pourcentage
-}
+
+// double calculPourcentage(int Pourcentage)
+// {
+//     return Pourcentage;
+// }
 
 int main(void)
 {
-    printf("===  RESUME  ==="); 
-    printf("Votre salaire est de "); 
-    printf("Total des dépenses"); 
-    printf("Reste"); 
-    printf("Vous avez dépensé 50\% de votre salaire (OK)"); 
+    double resultatSalaire = calculSalaire(0); //double resultatPourcentage = calculPourcentage(resultatPourcentage);
+    double resultatDepense = calculDepense(0); //double resultatRestant = calculReste(0);
+    printf("\n     ===  RESUME  ===\n"); 
+    printf("\nVotre salaire est de : %.2lf€\n", resultatSalaire); 
+    printf("Total des dépenses effectuées : %.2lf€\n", resultatDepense); 
+    //printf("Restant sur salaire : %.2lf€\n", resultatRestant);
+    // printf("Pourcentage fonds dépensé : %.0lf\%\n", resultatPourcentage); 
     return 0;
 }
