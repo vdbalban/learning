1049dcd5f75b0dc8f79cd9036771f133dd2cea1a
vdbalban <vdb.alban@gmail.com>
2026-07-18 21:13:03 +0200

    premier commit

---
+++ exodepense.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int calculSalaire(int salaire)
+{
+    printf("Quel est votre salaire ...")
+    scanf("%d", salaire);
+    return salaire;
+}
+
+int calculDepense(int depense)
+{
+    printf("Saisie de la depense ...")
+}
+int main(void)
+{
+    printf("===  RESUME  ==="); 
+    printf("Votre salaire est de "); 
+    printf("Total des dépenses"); 
+    printf("Reste"); 
+    printf("Vous avez dépensé % de votre salaire (OK)"); 
+    return 0;
+}
