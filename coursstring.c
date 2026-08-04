6679bdf8ca9f12458b39a8945a0b3446ac0f8e6f
vdbalban <vdb.alban@gmail.com>
2026-08-04 15:11:40 +0200

    cours

--- coursstring.c
+++ coursstring.c
@@ -3,20 +3,10 @@
 
 int	main(void)
 {
-	char	chaine[5];
+	char	chaine[] = "Salut";
 	int		i;
 
-	chaine[0] = 'S';
-	chaine[1] = 'A';
-	chaine[2] = 'L';
-	chaine[3] = 'U';
-	chaine[4] = 'T';
-	i = 0;
-	while (i < 5)
-	{
-		write (1, (chaine + i), 1);
-		write (1, "\n", 1);
-		i++;
-	}
+	write (1, chaine, 5);
+	write (1, "\n", 1);
 	return (0);
 }
