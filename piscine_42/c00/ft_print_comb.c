23986516eb8712589b71592bb22244392a8a4201
vdbalban <vdb.alban@gmail.com>
2026-08-06 09:53:04 +0200

    firstcommit

---
+++ piscine_42/c00/ft_print_comb.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_print_comb(void);
+
+void	ft_print_comb(void)
+{
+	char	n;
+
+	while (n < 999)
+	{
+		n = 0;
+		ssize_t write(int 1, n, 1);	
+	}
+}	
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
--- premierstrlen.c
+++ premierstrlen.c
@@ -10,9 +10,9 @@ int	main(void)
 	write(1, "Quel est votre prénom ?\n", 26);
 	scanf("%s", prenom);
 	tailleprenom = strlen(prenom);
-	write(1,"Bonjour et bienvenue a vous ",29);
-	write(1,prenom, tailleprenom);
-	write(1,"\n", 1);
+	write(1, "Bonjour et bienvenue a vous", 29);
+	write(1, prenom, tailleprenom);
+	write(1, "\n", 1);
 	return (0);
 }
 
